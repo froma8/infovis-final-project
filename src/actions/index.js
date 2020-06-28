ef113e5498888e433fe04eeba066ba5085526eb6
Marco Moauro <marco@wanderio.com>
2020-06-28 13:09:25 +0200

    add more info of each selected comunities (#2)
    * add more info of selected comunities
    
    * change indent size space

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,36 +1,43 @@
 import * as t from './types'
-import graph from '../data/dolphins.json'
 import { getCoordinates } from '../helpers'
-
 
 export const selectNode = (data) => ({ type: t.SELECT_NODE, data })
 
 export const loadGraph = (data) => (dispatch) => {
-    const graph = {}
-    getCoordinates(data)
-      .then(nodesCoordinates => {
-          graph.nodes = [...nodesCoordinates.values()]
+  const graph = {}
+  getCoordinates(data)
+    .then(nodesCoordinates => {
+      graph.nodes = [...nodesCoordinates.values()]
 
-          const newEdges = new Map()
-          data.edges.forEach(edge => {
-              const [first, second] = edge.split('-')
-              const nodeA = nodesCoordinates.get(first)
-              const nodeB = nodesCoordinates.get(second)
-              if (nodeA && nodeB) {
-                  newEdges.set(edge, { x1: nodeA.x, y1: nodeA.y, x2: nodeB.x, y2: nodeB.y, key: edge })
-              }
-          })
-          graph.edges = [...newEdges.values()]
-          graph.communities = []
-          data.communities.forEach((plex) => {
-              graph.communities.push(plex.map(node => nodesCoordinates.get(node.toString())))
-          })
-          dispatch({ type: t.LOAD_GRAPH, data: graph})
+      const newEdges = new Map()
+      data.edges.forEach(edge => {
+        const [first, second] = edge.split('-')
+        const nodeA = nodesCoordinates.get(first)
+        const nodeB = nodesCoordinates.get(second)
+        if (nodeA && nodeB) {
+          newEdges.set(edge, { x1: nodeA.x, y1: nodeA.y, x2: nodeB.x, y2: nodeB.y, key: edge })
+        }
       })
+      graph.edges = [...newEdges.values()]
+      graph.communities = []
+      data.communities.forEach((plex) => {
+        graph.communities.push(plex.map(node => nodesCoordinates.get(node.toString())))
+      })
+      dispatch({ type: t.LOAD_GRAPH, data: graph })
+    })
 }
 
 export const selectCommunities = data => {
-    //forse dovrei tener conto dei filtri
-    let communities = graph.communities.filter(community => community.includes(parseInt(data)))
-    return { type: t.SELECT_COMMUNITIES, data: communities }
+  //forse dovrei tener conto dei filtri
+  const label = data.label
+  const graphEdges = data.edges
+  const communitiesNodes = data.communities.filter(community => community.some(communityNode => communityNode.label === label))
+  let communities = communitiesNodes.map(communityNodes => {
+    let communityEdges = graphEdges.filter(edge => {
+      const [first, second] = edge.key.split('-')
+      return communityNodes.some(node => node.label === first) && communityNodes.some(node => node.label === second)
+    })
+    return { nodes: communityNodes, edges: communityEdges }
+  })
+  return { type: t.SELECT_COMMUNITIES, data: communities }
 }
--- src/components/Node.js
+++ src/components/Node.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import '../css/graph.css'
 import { connect } from 'react-redux'
-import { getSelectedNode } from '../reducers'
+import { getSelectedNode, getEdges, getCommunities } from '../reducers'
 import { selectNode, selectCommunities } from '../actions'
 
 const mapStateToProps = state => ({
-  selectedNode: getSelectedNode(state)
+  selectedNode: getSelectedNode(state),
+  edges: getEdges(state),
+  communities: getCommunities(state)
 })
 
-const selectNodeAndCommunities = (label, selectNode, selectCommunities) => {
+const selectNodeAndCommunities = (label, selectNode, selectCommunities, edges, communities) => {
   selectNode(label)
-  selectCommunities(label)
+  selectCommunities({label, edges, communities})
 }
 
-const Node = ({ x, y, size = 2, label, selectedNode, selectNode, selectCommunities }) => {
+const Node = ({ x, y, size = 2, label, selectedNode, selectNode, selectCommunities, edges, communities }) => {
   return (
     <>
-      <circle cx={x} cy={y} r={size} fill={selectedNode ===  label ? 'red' : 'black'} onClick={() => selectNodeAndCommunities(label, selectNode, selectCommunities)}/>
+      <circle cx={x} cy={y} r={size} fill={selectedNode ===  label ? 'red' : 'black'} onClick={() => selectNodeAndCommunities(label, selectNode, selectCommunities, edges, communities)}/>
       <text x={x} y={y + 4.5 } fill="white" textAnchor="middle" style={{ fontSize: '1rem' }}>{label}</text>
     </>
   )
